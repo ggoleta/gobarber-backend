60bef8ae35f819674754608d1ccc77059bbdc4a5
ggoleta <rogerio_contato@hotmail.com>
2020-09-20 17:59:44 -0300

    validando dados das rotas (celebrate)

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import SessionsControllers from '../controllers/SessionsControllers';
 
 const sessionsRouter = Router();
 const sessionsControllers = new SessionsControllers();
-// const usersRepository = new UsersRepository(); // precisamos jogar isso dentro da rota
 
-sessionsRouter.post('/', sessionsControllers.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsControllers.create,
+);
 
 export default sessionsRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import UsersControllers from '../controllers/UsersControllers';
@@ -11,7 +12,17 @@ const usersControllers = new UsersControllers();
 const userAvarController = new UserAvarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersControllers.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersControllers.create,
+);
 
 usersRouter.patch(
   '/avatar',
