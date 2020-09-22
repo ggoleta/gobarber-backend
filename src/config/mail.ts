b871d4a173243c31f07e866141c2d7ab7fcd67d8
ggoleta <rogerio_contato@hotmail.com>
2020-09-22 13:15:40 -0300

    email amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'equipe@bolissimos.com.br',
+      name: 'Equipe Bolíssimos',
+    },
+  },
+} as IMailConfig;
