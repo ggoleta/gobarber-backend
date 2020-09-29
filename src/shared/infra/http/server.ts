c1da933ad78d03e5580ddcf4972315c5a4a36d76
ggoleta <rogerio_contato@hotmail.com>
2020-09-29 20:36:00 -0300

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,16 +2,21 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
